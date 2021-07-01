a918744af5f2d9b21a4ce7eb11b3f2ea411c482a
NikolaiMGeorgiev <nikolaimgeorgiev@abv.bg>
2021-07-01 07:34:21 +0300

    Add files via upload

--- Mandelbrot.java
+++ Mandelbrot.java
@@ -6,20 +6,18 @@ public final class Mandelbrot implements Callable<List<Integer>> {
 
     private static final int MAX_ITERATIONS = 256;
 
-    int startX;
-    int startY;
     int lengthX;
     int lengthY;
     int g;
     int threads;
+    int threadId;
 
-    public Mandelbrot(int startX, int startY, int lengthX, int lengthY, int g, int threads) {
-        this.startX = startX;
-        this.startY = startY;
+    public Mandelbrot(int lengthX, int lengthY, int g, int threads, int threadId) {
         this.lengthX = lengthX;
         this.lengthY = lengthY;
         this.g = g;
         this.threads = threads;
+        this.threadId = threadId;
     }
 
     @Override
@@ -28,34 +26,44 @@ public final class Mandelbrot implements Callable<List<Integer>> {
         int currentThread = 0;
         for (int bx = 0; bx < threads; bx++) {
             for (int by = 0; by < g; by++) {
-                if (currentThread == 0) {
+                if (currentThread == threadId) {
+                    if (threadId == 1) {
+                        //System.out.println("bx: " + bx + " by: " + by + " tr: " + currentThread + " id" + threadId);
+                    }
                     for (int x = bx * lengthX; x < lengthX * (bx + 1); x++) {
                         for (int y = by * lengthY; y < lengthY * (by + 1); y++) {
+                            if (threadId == 1 ){
+                                //System.out.println("id: " + threadId + " x: " + x + " y: " + y);
+                            }
                             pixels.add(calculatePixel(x, y));
                         }
                     }
-                } else if (currentThread == threads - 1) {
+                }
+                if (currentThread == threads - 1) {
                     currentThread = -1;
                 }
                 currentThread++;
             }
         }
 
-        System.out.println("Finished mandelbrot at y = " + startY);
+        System.out.println("Finished mandelbrot with id = " + threadId);
         return pixels;
     }
 
     public int calculatePixel(int x, int y) {
-        int ix = 0;
-        int iy = 0;
+        double x0 = x * 3.5 / MultithreadingExample.WIDTH - 2.5;
+        double y0 = y * 2.0 / MultithreadingExample.HEIGHT - 1.0;
+        double ix = 0;
+        double iy = 0;
         int iteration = 0;
 
-        while (ix * ix + iy * iy < 4 && iteration < MAX_ITERATIONS) {
-            int xTemp = ix * ix - iy * iy + x;
-            iy = 2 * ix * iy + y;
+        while (ix * ix + iy * iy < 4.0 && iteration < MAX_ITERATIONS) {
+            double xTemp = ix * ix - iy * iy + x0;
+            iy = 2 * ix * iy + y0;
             ix = xTemp;
             iteration++;
         }
+
         return iteration;
     }
 }
--- MultithreadingExample.java
+++ MultithreadingExample.java
@@ -1,3 +1,8 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
@@ -6,8 +11,10 @@ import java.util.concurrent.*;
 
 public class MultithreadingExample {
 
-    public static final int WIDTH = 3840;
-    public static final int HEIGHT = 2160;
+   // public static final int WIDTH = 3840;
+    //public static final int HEIGHT = 2160;
+    public static final int WIDTH = 100;
+    public static final int HEIGHT = 60;
     private static final int ALL_PIXELS = WIDTH * HEIGHT;
 
     /*
@@ -18,8 +25,10 @@ public class MultithreadingExample {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         Instant start = Instant.now();
-        final int THREADS = Integer.parseInt(args[0]);
-        final int G = Integer.parseInt(args[1]);
+        //final int THREADS = Integer.parseInt(args[0]);
+        //final int G = Integer.parseInt(args[1]);
+        final int THREADS = 4;
+        final int G = 5;
         int sectorWidth = WIDTH / THREADS;
         int sectorHeight = HEIGHT / G;
 
@@ -30,18 +39,19 @@ public class MultithreadingExample {
         for (int thread = 0; thread < THREADS; thread++) {
             int startY = thread * sectorHeight;
             int startX = thread * sectorWidth;
-            futures.add(executorService.submit(new Mandelbrot(startX, startY, sectorWidth, sectorHeight, G, THREADS)));
+            futures.add(executorService.submit(new Mandelbrot(sectorWidth, sectorHeight, G, THREADS, thread)));
         }
+
 
         executorService.shutdown();
         executorService.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println("Took " + start.until(Instant.now(), ChronoUnit.MILLIS) + "ms");
 
         List<List<Integer>> allMandelbrots = new ArrayList<>();
         for (Future<List<Integer>> future : futures) {
             allMandelbrots.add(future.get());
         }
         drawMandelbrots(allMandelbrots, sectorWidth, sectorHeight, THREADS, G);
-        System.out.println("Took " + start.until(Instant.now(), ChronoUnit.MILLIS) + "ms");
     }
 
     private static void drawMandelbrots(List<List<Integer>> allMandelbrots, int lengthX, int lengthY, int threads, int g) {
@@ -59,22 +69,100 @@ public class MultithreadingExample {
         int currentThread = 0;
         int iterations = 0;
         int index = 0;
-        for (int bx = 0; bx < threads; bx++) {
-            for (int by = 0; by < g; by++) {
+        int i = 0;
+        int[][] matrix = new int[WIDTH][HEIGHT];
+        for (int x = 0; x < WIDTH; x++) {
+            StringBuilder output = new StringBuilder();
+            for (int y = 0; y < HEIGHT; y++) {
+                index = iterations * lengthY + i;
+                //System.out.println(currentThread + "| indx: " + index + " iter:" + iterations + " i: " + i + "L:" + lengthY + " H: " + y + " W:" + x);
+                output.append(allMandelbrots.get(currentThread).get(index) + " ");
+                matrix[x][y] = allMandelbrots.get(currentThread).get(index);
+                i++;
 
-                StringBuilder output = new StringBuilder();
-                for (int x = 0; x < WIDTH; x++) {
-                    for (int y =0; y < HEIGHT; y++) {
-                        index = iterations * lengthY + (x - )
-                    }
-                        output.append(allMandelbrots.get(currentThread).get(x) + " ");
-                    }
-                if (currentThread == threads - 1) {
-                    currentThread = -1;
+                if (i == lengthY) {
+                    //System.out.println("s: " + allMandelbrots.get(currentThread).size());
+                    currentThread++;
+                    i = 0;
+                }
+                if (currentThread == threads) {
+                    currentThread = 0;
                     iterations++;
                 }
-                currentThread++;
+
             }
+            //System.out.println(output.toString());
+        }
+
+        BufferedImage bi = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_3BYTE_BGR);
+
+        Graphics2D g2d = bi.createGraphics();
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, WIDTH, HEIGHT);
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                /*
+                int rgb = matrix[x][y] - 1;
+                rgb = (rgb << 8) + matrix[x][y] - 1;
+                rgb = (rgb << 8) + matrix[x][y] - 1;
+                */
+
+                matrix[x][y] -= 1;
+				if (matrix[x][y] == 0) { // inside ...
+					bi.setRGB(x, y, 0x00ff00);
+				} else if (matrix[x][y] <= 10) { // outside ... (rapid move)
+					bi.setRGB(x, y, 0xFFFFFF);
+				// close to inside cases ...
+				// } else if (10 < matrix[x][y] && matrix[x][y] <= 50) {
+				// bi.setRGB(x, y, 0x0033EE);
+				} else if (matrix[x][y] == 11) {
+					bi.setRGB(x, y, 0x0000ff);
+				} else if (matrix[x][y] == 12) {
+					bi.setRGB(x, y, 0x0000ee);
+				} else if (matrix[x][y] == 13) {
+					bi.setRGB(x, y, 0x0000dd);
+				} else if (matrix[x][y] == 14) {
+					bi.setRGB(x, y, 0x0000cc);
+				} else if (matrix[x][y] == 15) {
+					bi.setRGB(x, y, 0x0000bb);
+				} else if (matrix[x][y] == 16) {
+					bi.setRGB(x, y, 0x0000aa);
+				} else if (matrix[x][y] == 17) {
+					bi.setRGB(x, y, 0x000099);
+				} else if (matrix[x][y] == 18) {
+					bi.setRGB(x, y, 0x000088);
+				} else if (matrix[x][y] == 19) {
+					bi.setRGB(x, y, 0x000077);
+				} else if (matrix[x][y] == 20) {
+					bi.setRGB(x, y, 0x000066);
+				} else if (20 < matrix[x][y] && matrix[x][y] <= 30) {
+					bi.setRGB(x, y, 0x666600);
+				} else if (30 < matrix[x][y] && matrix[x][y] <= 40) {
+					bi.setRGB(x, y, 0x777700);
+				} else if (40 < matrix[x][y] && matrix[x][y] <= 50) {
+					bi.setRGB(x, y, 0x888800);
+				} else if (50 < matrix[x][y] && matrix[x][y] <= 100) {
+					bi.setRGB(x, y, 0x999900);
+				} else if (100 < matrix[x][y] && matrix[x][y] <= 150) {
+					bi.setRGB(x, y, 0xaaaa00);
+				} else if (150 < matrix[x][y] && matrix[x][y] <= 200) {
+					bi.setRGB(x, y, 0xbbbb00);
+				} else if (200 < matrix[x][y] && matrix[x][y] <= 250) {
+					bi.setRGB(x, y, 0xcccc00);
+				} else {
+					bi.setRGB(x, y, 0xeeee00);
+				}
+
+                //System.out.print(matrix[x][y]);
+            }
+            //System.out.println();
+        }
+
+        try {
+            ImageIO.write(bi, "PNG", new File("mandelbrot.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
